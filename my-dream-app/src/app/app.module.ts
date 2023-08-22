6566b04f102a16bac026a80bb1a043193436d47f
Tharushi <ththarushi3@gmail.com>
2023-08-22 14:38:46 +0530

    Srart work on angular essential training

---
+++ my-dream-app/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  //declarations: used to make components, directives, and pipes that available 
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
